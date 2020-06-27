6443f036a3962abc67d5a5009369be9dbc302190
ryo-beppu <ryo502045@gmail.com>
2020-06-27 18:11:37 +0900

    lint導入

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import SVG from './scripts/components/Opening'
-import "./sass/index.scss"
+import React from "react";
+import ReactDOM from "react-dom";
+import SVG from "./components/Opening";
+import "./sass/index.scss";
 
-ReactDOM.render(
-    <SVG />,
-  document.getElementById('app')
-);
+ReactDOM.render(<SVG />, document.getElementById("app"));
