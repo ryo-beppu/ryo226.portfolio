4989d66fd5e45f3d03e6f110405a6c71d3206a97
ryo-beppu <ryo502045@gmail.com>
2020-07-13 10:20:56 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ryo226.portfolio/precache-manifest.397ee888e6960066c10a0fdcc7aca91e.js"
+  "/ryo226.portfolio/precache-manifest.bd0fb00f77dbf5cd2e71d4a48b9a4bad.js"
 );
 
 self.addEventListener('message', (event) => {
